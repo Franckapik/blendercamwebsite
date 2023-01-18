f1ad427ef47195085549c571a437a95245faa481
Franckapik <fanch44@hotmail.com>
2023-01-18 14:59:38 +0100

    Home without link

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,15 @@
+import '@/styles/custom.scss'
 import '@/styles/globals.css'
-import 'bootstrap/dist/css/bootstrap.min.css';
+import { Quicksand } from "@next/font/google";
+
+const font = Quicksand({
+  subsets: ["latin"],
+});
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return( 
+  <div className={font.className}>
+<Component  {...pageProps} />
+  </div>
+  )
 }
--- pages/index.js
+++ pages/index.js
@@ -1,27 +1,63 @@
-import { Mulish } from "@next/font/google";
-import { Nav } from "react-bootstrap";
+import { Button, Col, Container, Nav, Row } from "react-bootstrap";
 
-const mulish = Mulish({
-  subsets: ["latin"],
-});
+
 
 export default function Home() {
   return (
-    <main className={mulish.className} data-bs-theme="dark">
-   <Nav  className="justify-content-space-between navbar" activeKey="/home">
+    <main className="main">
+<Container fluid className="d-flex flex-column home_container">
+        <Row>
+          <Nav className="justify-content-around home_navbar" activeKey="/home">
         <Nav.Item>
           <Nav.Link href="/home">Home</Nav.Link>
         </Nav.Item>
         <Nav.Item>
-          <Nav.Link eventKey="link-1">Feature</Nav.Link>
+          <Nav.Link eventKey="link-1">Features</Nav.Link>
         </Nav.Item>
         <Nav.Item>
           <Nav.Link eventKey="link-2">Documentation</Nav.Link>
         </Nav.Item>
         <Nav.Item>
-          <Nav.Link >Contributing</Nav.Link>
+          <Nav.Link>Contributing</Nav.Link>
         </Nav.Item>
       </Nav>
+      </Row>
+      
+
+      <Row className="align-items-center h-100 text-center">
+        <Col sm={2} className=" d-flex flex-column  home_discussion">
+        <img
+            src="/element.svg"
+            alt="The logo of element"
+          />
+          <img
+            src="/discord.svg"
+            alt="The logo of discord"
+          />
+          <img
+            src="/github.svg"
+            alt="The logo of github"
+          />
+        </Col>
+        <Col sm={8} >
+ 
+          <img
+            src="/logo.svg"
+            alt="The logo of BlenderCam"
+            className="home_logo"
+          />
+          <p className="home_logo_sentence">
+            Free and open-source solution for Computer Aided Manufacturing with
+            Blender3D
+          </p>
+        </Col>
+        <Col sm={2}></Col>
+      </Row>
+      <Row className="justify-content-around">
+        <div className="home_started_button text-center"><Button className="btn-outline-blender" >Get Started</Button></div>
+
+      </Row>
+</Container>
     </main>
   );
 }
