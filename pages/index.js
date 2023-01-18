1cb92b611fd8767d30b599d041aef2e1ea29b7cf
Franckapik <fanch44@hotmail.com>
2023-01-18 11:09:34 +0100

    simple navbar without css module

--- pages/index.js
+++ pages/index.js
@@ -1,31 +1,27 @@
-import css from "../styles/Home.module.css";
-import { Canvas } from "@react-three/fiber";
-import Logo from "../components/Logo";
-import Button from "react-bootstrap/Button";
 import { Mulish } from "@next/font/google";
+import { Nav } from "react-bootstrap";
+
 const mulish = Mulish({
   subsets: ["latin"],
 });
 
 export default function Home() {
   return (
-    <main className={mulish.className}>
-      <div className={css.scene}>
-        <Canvas
-          shadows={true}
-          className={css.canvas}
-          camera={{
-            position: [-10, 5, 5],
-          }}
-        >
-          <Logo route={"/"} position={[0, 2, 0]} />
-          <ambientLight />
-        </Canvas>
-      </div>
-      <div className={css.overlay3D}>
-        <p className={css.sentence}>My first Nextjs + React-Three-Fiber page! </p>
-        <Button>Get started</Button>
-      </div>
+    <main className={mulish.className} data-bs-theme="dark">
+   <Nav  className="justify-content-space-between navbar" activeKey="/home">
+        <Nav.Item>
+          <Nav.Link href="/home">Home</Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Nav.Link eventKey="link-1">Feature</Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Nav.Link eventKey="link-2">Documentation</Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Nav.Link >Contributing</Nav.Link>
+        </Nav.Item>
+      </Nav>
     </main>
   );
 }
